4885ce19499c363f1a505fa6cdcfe3c33a03b4ba
BoitsovRich <53099301+Boitsovrich@users.noreply.github.com>
2020-09-04 13:52:39 +0300

    Назначение ежемесячной выплаты студенческой семье Санкт-Петертербурга
    каскадная модель
    1. Сбор требований
    2. Проектирование
    3. Реализация
    4. Тестирование
    5. Внедрение
    
    использую спиральную модель проектирования

---
+++ src/saveStudentOrder.java
@@ -0,0 +1,27 @@
+public class saveStudentOrder {
+    public static void main(String[] args) {
+       checkAll();
+    }
+    static void checkAll(){
+        checkCityRegister();
+        checkWedding();
+        checkChildren();
+        checkChildren();
+        checkStudent();
+    }
+
+    static void checkCityRegister(){
+        System.out.println("CityRegister is running");
+    }
+
+    static void checkWedding () {
+        System.out.println("Wedding is running");
+    }
+    static void checkChildren () {
+        System.out.println("Children check is running");
+    }
+
+    static void checkStudent() {
+        System.out.println("Студенты проверяются");
+    }
+}
